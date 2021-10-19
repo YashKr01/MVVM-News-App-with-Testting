eca9fe5bb66550947f10ab41a7679522fe6ae9ba
YashKr01 <yashkumar201301@gmail.com>
2021-10-19 15:19:29 +0530

    Navigation Setup

--- app/src/main/java/com/example/newsnow/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsnow/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.newsnow.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.example.newsnow.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
     }
 }
--- app/src/main/java/com/example/newsnow/utils/Constants.kt
+++ app/src/main/java/com/example/newsnow/utils/Constants.kt
@@ -1,4 +1,8 @@
 package com.example.newsnow.utils
 
 object Constants {
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "9fb8214bfe174c1c85baf56e2da9a062"
+
 }

f035c4d471241abd43372e18509010a4d4456203
YashKr01 <yashkumar201301@gmail.com>
2021-10-19 16:12:57 +0530

    Extensions Functions

--- app/src/main/java/com/example/newsnow/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/example/newsnow/utils/ExtensionFunctions.kt
@@ -1,4 +1,24 @@
 package com.example.newsnow.utils
 
+import android.view.View
+import android.widget.Button
+
 object ExtensionFunctions {
+
+    fun View.show() {
+        this.visibility = View.VISIBLE
+    }
+
+    fun View.hide() {
+        this.visibility = View.INVISIBLE
+    }
+
+    fun Button.enable() {
+        this.isEnabled = true
+    }
+
+    fun Button.disable() {
+        this.isEnabled = false
+    }
+
 }

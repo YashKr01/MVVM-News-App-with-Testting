2aa19d577a116a35d14f6b723c482d88935f3d42
YashKr01 <yashkumar201301@gmail.com>
2022-04-14 23:36:02 +0530

    -> Code Refactoring

--- app/src/main/java/com/example/newsnow/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsnow/viewmodels/NewsViewModel.kt
@@ -4,24 +4,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
-import androidx.paging.liveData
 import com.example.newsnow.data.database.NewsArticle
 import com.example.newsnow.data.database.NewsArticleDao
-import com.example.newsnow.data.network.paging.NewsPagingSource
 import com.example.newsnow.data.network.retrofit.ApiInterface
 import com.example.newsnow.data.network.retrofit.NewsRepository
 import com.example.newsnow.utils.Constants.SPORTS
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@Suppress("OPT_IN_IS_NOT_ENABLED")
-@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class NewsViewModel @Inject constructor(
     private val repository: NewsRepository,
